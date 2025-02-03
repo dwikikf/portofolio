fbb7b627eb12992abbf0d79ae6691a4ad6102bb1
dwikikf <dwikikautsarfahmist@gmail.com>
2025-02-03 09:56:27 +0700

    tambah sampel assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline tracking-tight">Hello World</h1>
+      <h1 className="text-3xl font-bold underline tracking-tight"> Hello World </h1>
     </>
   )
 }
